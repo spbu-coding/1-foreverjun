979cca406105fdca7aedce37f7a2c76d7ba55d45
matvey <st090604@student.spbu.ru>
2020-10-02 01:18:31 +0300

    final code 2

--- src/main.c
+++ src/main.c
@@ -3,33 +3,35 @@
 #include <math.h>
 #define EXPERIMENT_NUMBER 6
 #define MAX_CELL_SIZE 30
-#define PI 3.14592653
+#define PI 3.141592653
 #define error(...) (fprintf(stderr,__VA_ARGS__))
-double simpson_method(int number,double left_border,double right_border){
+double simpson_method(int number_of_exp,double left_border,double right_border){
   double interval,result=0;
-  for(int i=0;i<number;i++){
-      interval= (right_border-left_border) /number;
-      result+=interval/6*(sin(left_border+i*interval)+4*sin((left_border+i*interval+right_border-(interval*(number-1-i)))/2)+sin(right_border-(interval*(number-1-i))));
+  for(int i=0;i<number_of_exp;i++){
+      interval= (right_border-left_border) /number_of_exp;
+      result+= interval / 6 * (sin(left_border + i * interval) + 4 * sin((left_border + i * interval + right_border -
+                                                                            (interval * (number_of_exp - 1 - i))) / 2) +
+                                 sin(right_border - (interval * (number_of_exp - 1 - i))));
   }
   return result;
 }
-double rectangles_method(int number,double left_border,double right_border) {
+double rectangles_method(int number_of_exp,double left_border,double right_border) {
     int i;
     double interval, result = 0;
-    interval = (right_border - left_border) / number;
-    for (i = 0; i < number; i++) {
+    interval = (right_border - left_border) / number_of_exp;
+    for (i = 0; i < number_of_exp; i++) {
         result += sin(left_border + interval / 2 + interval * i);
     }
     result = result * interval;
     return result;
 }
-void free_result (char** result,int experiments_count){
+void free_string_array (char** result,int experiments_count){
     for (int i = 0; i <experiments_count ; ++i) {
         free(result[i]);
     }
     free(result);
 }
-char** processing(int *partition_sizes,double left_border,double right_border) {
+char** fills_in_array(int *partition_counts,double left_border,double right_border) {
     int i;
     char **result=(char**)malloc(sizeof(char*)*EXPERIMENT_NUMBER);
     if(!result){
@@ -39,11 +41,15 @@ char** processing(int *partition_sizes,double left_border,double right_border) {
     for (i = 0; i < EXPERIMENT_NUMBER; i++) {
         result[i]=(char*)malloc(sizeof(char)*MAX_CELL_SIZE);
         if(!result[i]){
-            free_result(result,i);
             error("Can not allocate memory for result[%d]\n",i);
+            free_string_array(result,i);
             return NULL;
         }
-        if(!sprintf(result[i], "%d %.5f %.5f", *(partition_sizes + i),rectangles_method(*(partition_sizes + i),left_border,right_border),simpson_method(*(partition_sizes + i),left_border,right_border))){
+
+        if(!sprintf(result[i], "%d %.5f %.5f", *(partition_counts + i),
+                    rectangles_method(*(partition_counts + i), left_border, right_border),
+                    simpson_method(*(partition_counts + i), left_border, right_border))){
+            free_string_array(result,i+1);
             error("Can not write string to result[%d]\n",i);
             return NULL;
         }
@@ -59,16 +65,16 @@ int read_interval ( double *left_border,double *right_border){
         error("Cannot read left border");
         return -1;
     }
+    if (*left_border<0){
+        error("Left border less than 0");
+        return -1;
+    }
     if (printf("Enter right border: ")<0){
         error("Cannot write to stdout");
         return -1;
     }
     if (scanf("%lf",right_border)!=1){
         error("Cannot read right border");
-        return -1;
-    }
-    if (*left_border<0){
-        error("Left border less than 0");
         return -1;
     }
     if (*right_border>PI){
@@ -86,17 +92,18 @@ int main() {
     if(read_interval(&left_border,&right_border)){
         return 1;
     }
-    int partition_sizes[] ={6,10,20,100,500,1000};
-    char** result = processing(partition_sizes,left_border,right_border);
+    int partition_counts[EXPERIMENT_NUMBER] ={6,10,20,100,500,1000};
+    char** result = fills_in_array(partition_counts,left_border,right_border);
     if(!result) {
         return 1;
     }
     for (int i = 0; i < EXPERIMENT_NUMBER; ++i) {
         if(printf("%s\n",result[i])<0){
             error("Can not write result[%d] to stdout",i);
-            break;
+            free_string_array(result,EXPERIMENT_NUMBER);
+            return 1;
         }
     }
-    free_result(result,EXPERIMENT_NUMBER);
+    free_string_array(result,EXPERIMENT_NUMBER);
     return 0;
 }
